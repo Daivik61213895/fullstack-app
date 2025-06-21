141edbe728a0332729ebc810cc3cb1cfd6882c66
Daivik Kansara <daivik612@gmail.com>
2025-06-21 19:20:14 +0530

    Make api connection to dynamic

--- fullstack-app-fe/src/api.js
+++ fullstack-app-fe/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://54.210.227.176:5000/api', // Replace with your backend URL(use localhost instead of IP if running locally)
+    baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/api', // Fallback to localhost if not set
 });
 
 export default api;
